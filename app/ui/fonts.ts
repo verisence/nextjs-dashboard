543272042300f3fcb165002215d4fc6bfa5be398
Phil Kipkemboi <kipkemboiphil@gmail.com>
2024-03-22 09:50:59 +0300

    fix required lusitana export in ui/fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,7 @@
-import { Inter, Maven_Pro, Shadows_Into_Light } from 'next/font/google'
+import { Inter, Maven_Pro, Shadows_Into_Light, Lusitana } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] })
 export const maven = Maven_Pro({ subsets: ['latin'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const shadows = Shadows_Into_Light({ weight: '400', subsets: ['latin'] })
