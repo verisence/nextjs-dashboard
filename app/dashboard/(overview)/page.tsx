efb5a0b7a49ca52e7889b962642339f163357273
Phil Kipkemboi <kipkemboiphil@gmail.com>
2024-04-02 09:44:21 +0300

    add loading skeletons and the customers and invoices pages

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,8 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data";
-import { Card } from "../ui/dashboard/cards";
-import LatestInvoices from "../ui/dashboard/latest-invoices";
-import RevenueChart from "../ui/dashboard/revenue-chart";
-import { lusitana } from "../ui/fonts";
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "@/app/lib/data";
+import { Card } from "@/app/ui/dashboard/cards";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { lusitana } from "@/app/ui/fonts";
 
 export default async function Page() {
     const revenue = await fetchRevenue()
